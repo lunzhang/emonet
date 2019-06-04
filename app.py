443e0ae594315dea6b0115832fb11fdb3e62d5d5
lunzhang <lun.zhang1993@gmail.com>
2019-06-04 17:02:26 -0400

    Add analysis endpoint

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from net import analysis
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
     return render_template('index.html')
+
+@app.route('/analysis', methods=['POST'])
+def emotion():
+    result = analysis(request.data)
+    return result
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')

679b240858c859ee1a47f8c044393546d6b9a0fe
lunzhang <lun.zhang1993@gmail.com>
2019-06-18 15:04:14 -0400

    Add port

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from net import analysis
+import os
 
 app = Flask(__name__)
 
@@ -14,4 +15,5 @@ def emotion():
     return jsonify(result)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port)

f411933c48be33f17c0189e29dab4080eea8e03a
lunzhang <lun.zhang1993@gmail.com>
2019-06-18 15:24:49 -0400

    Fix Syntax Error

--- app.py
+++ app.py
@@ -16,4 +16,5 @@ def emotion():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 80))
-    app.run(debug=True, host='0.0.0.0', port)
+    host = '0.0.0.0'
+    app.run(debug=True, host, port)

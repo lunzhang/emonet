618df8c239d9c7ee19598d1fc542b07dac6bcfc4
lunzhang <lun.zhang1993@gmail.com>
2019-06-18 15:19:37 -0400

    Port

--- Dockerfile
+++ Dockerfile
@@ -6,3 +6,4 @@ WORKDIR /app
 RUN pip install -r requirements.txt
 ENTRYPOINT ["python"]
 CMD ["app.py"]
+EXPOSE 80
--- app.py
+++ app.py
@@ -15,5 +15,5 @@ def emotion():
     return jsonify(result)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 80))
     app.run(debug=True, host='0.0.0.0', port)
